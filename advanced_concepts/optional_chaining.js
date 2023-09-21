4cb7a74df130735992104e103461eafb9f04ed46
vandriodd <ludasaadi@gmail.com>
2023-09-21 20:15:45 +0000

    Optional chaining

---
+++ advanced_concepts/optional_chaining.js
@@ -0,0 +1,27 @@
+// Optional chaining → allows to call object properties safely, without throwing an error
+
+const obj = undefined;
+
+/* console.log(obj.hello); */ // TypeError: Cannot read properties of undefined (reading 'hello')
+
+// ↓ Old ways of checking for undefined ↓
+if (obj) {
+  obj.hello;
+}
+
+obj && obj.hello;
+
+// ↓ Modern one, using optional chaining ↓
+obj?.hello; // returns undefined without throwing an error
+
+// ↓ Optional chaining in arrays ↓
+const arr = [1, 2, 3];
+
+console.log(arr?.[0]); // If the array is undefined, returns undefined, else gets the first element
+
+// ↓ Optional chaining in functions ↓
+// If foo is defined, calls fo with arguments 1 and 2
+function foo(a, b) {
+  return a + b;
+}
+console.log(foo?.(1, 2)); // 3
