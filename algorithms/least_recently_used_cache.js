9403ed69da096c416d1bd30018f87ff34c3e223f
vandriodd <ludasaadi@gmail.com>
2023-09-25 19:36:01 +0000

    Remove test on LRU

--- algorithms/least_recently_used_cache.js
+++ algorithms/least_recently_used_cache.js
@@ -7,39 +7,35 @@
 // - `putItem` Create or update a key value pair in the cache. Evict the least recently used key if the size of keys exceeds the max capacity
 
 export class LRU {
-	constructor(capacity) {
-		this.capacity = capacity;
-		this.cache = new Map();
-	}
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.cache = new Map();
+  }
 
-	getItem(key) {
-		const item = this.cache.get(key);
+  getItem(key) {
+    const item = this.cache.get(key);
 
-		// Map keeps track of insertion order, this will refresh the item
-		if (item) {
-			this.cache.delete(key);
-			this.cache.set(key, item);
-		}
-		return item;
-	}
+    // Map keeps track of insertion order, this will refresh the item
+    if (item) {
+      this.cache.delete(key);
+      this.cache.set(key, item);
+    }
+    return item;
+  }
 
-	putItem(key, val) {
-		// delete to refresh the insertion order
-		if (this.cache.has(key)) {
-			this.cache.delete(key);
-		}
-		// evict the oldest item in the cache
-		else if (this.cache.size == this.capacity) {
-			this.cache.delete(this.oldestItem);
-		}
-		this.cache.set(key, val);
-	}
+  putItem(key, val) {
+    // delete to refresh the insertion order
+    if (this.cache.has(key)) {
+      this.cache.delete(key);
+    }
+    // evict the oldest item in the cache
+    else if (this.cache.size == this.capacity) {
+      this.cache.delete(this.oldestItem);
+    }
+    this.cache.set(key, val);
+  }
 
-	get oldestItem() {
-		return this.cache.keys().next().value;
-	}
+  get oldestItem() {
+    return this.cache.keys().next().value;
+  }
 }
-
-const cache = new LRU(5);
-cache.putItem('a', 1);
-cache.getItem('a');
