87d31bcd1930f54a8ec67172b9275d0ac84b9112
vandriodd <ludasaadi@gmail.com>
2024-04-28 12:00:00 -0300

    Other ways for functionality

--- modal/main.js
+++ modal/main.js
@@ -2,6 +2,10 @@ const openBtn = document.querySelector(".open-btn")
 const closeBtn = document.querySelector(".close-btn")
 const modal = document.querySelector(".modal-overlay")
 
+// document.querySelector(".open-btn").click = function()
+
+// openBtn.onclick = () => modal.setAttribute("open", "")
+
 openBtn.addEventListener("click", () => {
   modal.setAttribute("open", "")
 })
