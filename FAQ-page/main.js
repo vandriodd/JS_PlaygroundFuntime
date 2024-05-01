9d6aa1f5d19eaeab2899994d30202a71537554b3
vandriodd <ludasaadi@gmail.com>
2024-05-01 17:47:29 -0300

    Toggle show-text via closest method

--- FAQ-page/main.js
+++ FAQ-page/main.js
@@ -1,9 +1,19 @@
-const btns = document.querySelectorAll(".question-btn");
+// const btns = document.querySelectorAll(".question-btn");
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const question = e.currentTarget.parentElement.parentElement;
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
 
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+const container = document.querySelector(".section-center");
+
+container.addEventListener("click", (e) => {
+  const btn = e.target.closest(".question-btn");
+  if (btn) {
+    const question = btn.closest(".question");
     question.classList.toggle("show-text");
-  });
-});
+  }
+})
