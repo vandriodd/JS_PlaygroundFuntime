ddeb5884efae80f13fb81df48776034e89efffd3
vandriodd <ludasaadi@gmail.com>
2024-05-02 19:24:43 -0300

    Add multiple accordion toggle methods (commented)

--- FAQ-page/main.js
+++ FAQ-page/main.js
@@ -1,13 +1,31 @@
-// const btns = document.querySelectorAll(".question-btn");
+//* 1. Adding individual event listeners to each button
+// const btns = document.querySelectorAll(".question-btn")
 
 // btns.forEach((btn) => {
 //   btn.addEventListener("click", (e) => {
-//     const question = e.currentTarget.parentElement.parentElement;
+//     const question = e.currentTarget.parentElement.parentElement
 
+//     question.classList.toggle("show-text")
+//   })
+// })
+
+//* 2. Toggle each question individually
+// const questions = document.querySelectorAll(".question")
+
+// questions.forEach((question) => {
+//   const btn = question.querySelector(".question-btn")
+
+//   btn.addEventListener("click", () => {
+//     questions.forEach((item) => {
+//       if (item !== question) {
+//         item.classList.remove("show-text");
+//       }
+//     })
 //     question.classList.toggle("show-text");
-//   });
-// });
+//   })
+// })
 
+//* 3. Using event delegation on the Container
 const container = document.querySelector(".section-center");
 
 container.addEventListener("click", (e) => {
